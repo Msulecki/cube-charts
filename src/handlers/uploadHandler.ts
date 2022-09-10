611dd4b9ec49128b8719b7d4f43a67f1c1c5d4f3
Matt Sulecki <mat.sulecki@gmail.com>
2022-09-10 19:45:12 +0200

    add filename under upload input

--- src/handlers/uploadHandler.ts
+++ src/handlers/uploadHandler.ts
@@ -2,6 +2,26 @@ const uploadHandler = () => {
   const uploadForm = document.getElementById('upload-form') as HTMLFormElement;
   const fileInput = document.getElementById('file-input') as HTMLInputElement;
 
+  const handleFileSelect = (e: any) => {
+    const fileName: string = e.target?.files[0]?.name;
+
+    if (!fileName) {
+      return;
+    }
+
+    const uploadButton = document.getElementById('upload-submit');
+    uploadButton.classList.add('enabled');
+
+    const textEnd = fileName.slice(-8);
+    const textStart = fileName.slice(0, -8);
+
+    const textStartContainer = document.getElementById('filename-start');
+    const textEndContainer = document.getElementById('filename-end');
+
+    textStartContainer.textContent = textStart;
+    textEndContainer.textContent = textEnd;
+  };
+
   return new Promise((resolve, reject) => {
     const handleSubmit = (e: SubmitEvent) => {
       e.preventDefault();
@@ -26,7 +46,9 @@ const uploadHandler = () => {
         const onError = (e: ProgressEvent<FileReader>) => {
           reader.removeEventListener('load', onLoad);
           reader.removeEventListener('error', onError);
+
           uploadForm?.removeEventListener('submit', handleSubmit);
+          uploadForm?.removeEventListener('change', handleFileSelect);
 
           console.error('errored', e);
           reject(e);
@@ -40,6 +62,7 @@ const uploadHandler = () => {
     };
 
     uploadForm?.addEventListener('submit', handleSubmit);
+    uploadForm?.addEventListener('change', handleFileSelect);
   });
 };
 
